6d39fd6051a1750b78738dfdcbd829e187310280
Anton Serdyuchenko <anton415@gmail.com>
2016-09-16 10:09:00 +0300

    5.3 Removing duplicates from array. version 2

--- src/main/java/aserdyuchenko/WithoutDuplicates.java
+++ src/main/java/aserdyuchenko/WithoutDuplicates.java
@@ -1,6 +1,8 @@
 package aserdyuchenko;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * 5.3. Создание программу удаления дубликатов в массиве.
@@ -8,16 +10,27 @@ import java.util.ArrayList;
  *
  * @author Anton Serdyuchenko
  * @since 12.09.2016
- * @version 1.0
+ * @version 2.0
  *
  */
 public class WithoutDuplicates {
-   ArrayList withoutDuplicates(ArrayList array){
-       for (int i = array.size() - 1; i >= 0; i--){
-           for (int j = i - 1; j>=0; j--) {
-               if (array.get(i) == array.get(j)) array.remove(j);
-           }
-       }
-       return array;
+    List<String> withoutDuplicates(String[] array) {
+        /**
+         * Removing duplicates:
+         */
+        HashSet<String> hash = new HashSet<String>();
+        for (int index = 0; index < array.length; index++) {
+            array[index] = hash.add(array[index]) ? array[index] : null;
+        }
+        /**
+         * Remove empty elements:
+         */
+        List<String> cleanArray = new ArrayList<String>();
+        for (String s : array) {
+            if (s != null && s.length() > 0) {
+                cleanArray.add(s);
+            }
+        }
+        return cleanArray;
    }
 }
--- src/test/java/aserdyuchenko/WithoutDuplicatesTest.java
+++ src/test/java/aserdyuchenko/WithoutDuplicatesTest.java
@@ -1,7 +1,9 @@
 package aserdyuchenko;
 
 import org.junit.Test;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,21 +14,8 @@ import static org.junit.Assert.assertThat;
 public class WithoutDuplicatesTest {
     @Test
     public void whenRemoveDuplicatesFromAnArray() {
-        WithoutDuplicates without = new WithoutDuplicates();
-        ArrayList array = new ArrayList();
-        array.add("what");
-        array.add("what");
-        array.add("where");
-        array.add("where");
-        array.add("where");
-        array.add("where");
-        array.add("where");
-        array.add("when");
-        ArrayList result = without.withoutDuplicates(array);
-        ArrayList cleanArray = new ArrayList();
-        cleanArray.add("what");
-        cleanArray.add("where");
-        cleanArray.add("when");
-        assertThat(result, is(cleanArray));
+        WithoutDuplicates withoutDuplicates = new WithoutDuplicates();
+        List<String> result = withoutDuplicates.withoutDuplicates(new String[] { "1", "2", "2", "2", "3" });
+        assertThat(result, is(Arrays.asList(new String[]{"1", "2", "3"})));
     }
 }
