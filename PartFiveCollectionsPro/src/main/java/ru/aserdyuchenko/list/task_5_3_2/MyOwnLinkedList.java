a603b757d1f2d5a4eff02e817920e1ca58296467
anton415 <anton415@gmail.com>
2017-05-26 21:27:52 +0300

    Add new task (Realization of LinkedList), #159.

---
+++ PartFiveCollectionsPro/src/main/java/ru/aserdyuchenko/list/task_5_3_2/MyOwnLinkedList.java
@@ -0,0 +1,123 @@
+package ru.aserdyuchenko.list.task_5_3_2;
+
+import ru.aserdyuchenko.list.task_5_3_1.SimpleContainer;
+import java.util.Iterator;
+
+/**
+ * @author anton.
+ * My own linked list.
+ * @param <E> - E.
+ */
+public class MyOwnLinkedList<E> implements SimpleContainer<E> {
+    /**
+     * @param counter - counter.
+     */
+    private int counter = 0;
+    /**
+     * @param head - head.
+     */
+    private Node head;
+    /**
+     * @param current - current.
+     */
+    private Node current;
+
+    @Override
+    public void add(E e) {
+        if (head == null) {
+            head = new Node(e);
+        }
+
+        Node temp = new Node(e);
+        current = head;
+
+        if (current != null) {
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(temp);
+        }
+        counter++;
+    }
+
+    @Override
+    public E get(int index) {
+        if (head != null) {
+            current = head.getNext();
+            for (int i = 0; i < index; i++) {
+                if (current.getNext() == null) {
+                    return null;
+                }
+                current = current.getNext();
+            }
+            return (E) current.getData();
+        }
+        return (E) current;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        Iterator<E> it = new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < counter;
+            }
+
+            @Override
+            public E next() {
+                return (E) current.getData();
+            }
+        };
+        return it;
+    }
+
+}
+
+/**
+ * Node.
+ */
+class Node {
+    /**
+     * @param next - next.
+     */
+    private Node next;
+    /**
+     * @param data - data.
+     */
+    private Object data;
+
+    /**
+     * Constructor.
+     * @param dataVale - data value.
+     */
+    Node(Object dataVale) {
+        next = null;
+        data = dataVale;
+    }
+
+    /**
+     * Get data.
+     * @return - data.
+     */
+    public Object getData() {
+        return  data;
+    }
+
+    /**
+     * Get next.
+     * @return - next.
+     */
+    public Node getNext()  {
+        return next;
+    }
+
+    /**
+     * Set next.
+     * @param nextValue - next value.
+     */
+    public void setNext(Node nextValue) {
+        next = nextValue;
+    }
+}
---
+++ PartFiveCollectionsPro/src/test/java/ru/aserdyuchenko/list/task_5_3_2/MyOwnLinkedListTest.java
@@ -0,0 +1,59 @@
+package ru.aserdyuchenko.list.task_5_3_2;
+
+import org.junit.Test;
+import java.util.Iterator;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author anton.
+ */
+public class MyOwnLinkedListTest {
+    /**
+     * @param list - list.
+     */
+    private MyOwnLinkedList<String> list = new MyOwnLinkedList<>();
+
+    /**
+     * Test. Add and get.
+     */
+    @Test
+    public void whenAddAndGetValue() {
+        list.add("OK");
+        String result = list.get(0);
+        assertThat(result, is("OK"));
+    }
+
+    /**
+     * Test. Add and get.
+     */
+    @Test
+    public void whenAddThreeValueAndGetThird() {
+        list.add("OK1");
+        list.add("OK2");
+        list.add("OK3");
+        String result = list.get(2);
+        assertThat(result, is("OK3"));
+    }
+    /**
+     * Test iterator. Get next.
+     */
+    @Test
+    public void whenGetNextUseIterator() {
+        list.add("OK");
+        Iterator iterator = list.iterator();
+        Object result = iterator.next();
+        assertThat(result, is("OK"));
+    }
+
+    /**
+     * Test iterator. Check hasNext.
+     */
+    @Test
+    public void whenCheckHasNextUseIterator() {
+        list.add("OK");
+        Iterator iterator = list.iterator();
+        boolean result = iterator.hasNext();
+        assertThat(result, is(true));
+    }
+}
