4f141c230d8e46c9e2e641c76d059637d4a200ba
anton415 <anton415@gmail.com>
2017-08-13 15:42:58 +0300

    Add new methods in JdbcStorage.java: add(int n), print(), delete(), isTableExist().  Add TODO list.  Add new tests in JdbcStorageTest.java.

--- Part_Eight_SQL_JDBC/src/main/java/ru.aserdyuchenko/JDBC/JdbcStorage.java
+++ Part_Eight_SQL_JDBC/src/main/java/ru.aserdyuchenko/JDBC/JdbcStorage.java
@@ -4,36 +4,95 @@ import java.sql.*;
 
 /**
  * @author Anton Serdyuchenko. anton415@gmail.com
+ * TODO shift all .close() in finally.
+ * TODO add method create table.
+ * TODO add since tag in javaDoc.
  */
 public class JdbcStorage {
     private final Connection connection;
 
+    /**
+     * Constructor.
+     */
     public JdbcStorage() {
         final Settings settings = Settings.getInstance();
         try {
-            this.connection = DriverManager.getConnection(settings.value("jdbc.url"), settings.value("jdbc.username"), settings.value("jdbc.password"));
+            this.connection = DriverManager.getConnection(
+                    settings.value("jdbc.url"),
+                    settings.value("jdbc.username"),
+                    settings.value("jdbc.password"));
         } catch (SQLException e) {
             throw new IllegalStateException(e);
+        } finally {
+
         }
     }
 
-    public void get() {
+    /**
+     * Print all numbers in table.
+     */
+    public void print() {
         try (final Statement statement = this.connection.createStatement();
             final ResultSet rs = statement.executeQuery("select * from test")) {
             while (rs.next()) {
-                System.out.println(rs.getInt("number"));
+                System.out.println(String.format("%s", rs.getInt("number")));
             }
+            rs.close();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+
         }
     }
 
-    public void add(int n) {
-        try (final PreparedStatement statement = this.connection.prepareStatement("insert into test values(?)")) {
-            statement.setInt(1, 1);
-            statement.executeUpdate();
+    /**
+     * Delete all frome table "test".
+     */
+    public void delete() {
+        try {
+            final Statement statement = this.connection.createStatement();
+            statement.execute("delete from test");
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    /**
+     * Add numbers in table. From 1 to N.
+     * @param n - count numbers.
+     */
+    public void add(int n) {
+        delete();
+        try (final PreparedStatement statement = this.connection.prepareStatement("insert into test values(?)")) {
+            for (int i = 1; i <= n; i++) {
+                statement.setInt(1, i);
+                statement.executeUpdate();
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    /**
+     * Check if table exist.
+     */
+    public boolean ifTableExist() {
+        try {
+            DatabaseMetaData md = connection.getMetaData();
+            md.getTables(null, null, "test", null);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+
         }
     }
 }
--- Part_Eight_SQL_JDBC/src/test/java/ru/aserdyuchenko/JDBC/JdbcStorageTest.java
+++ Part_Eight_SQL_JDBC/src/test/java/ru/aserdyuchenko/JDBC/JdbcStorageTest.java
@@ -1,21 +1,34 @@
 package ru.aserdyuchenko.JDBC;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Anton Serdyuchenko. anton415@gmail.com
+ * TODO add assert in all tests.
  */
 public class JdbcStorageTest {
     private JdbcStorage storage = new JdbcStorage();
 
     @Test
     public void whenPrintAllTable() {
-        storage.get();
+        storage.print();
     }
 
     @Test
-    public void whenAddNumberInTable() {
-        storage.add(1);
+    public void whenAddNumberInTableAndPrintTable() {
+        storage.add(3);
+        storage.print();
     }
 
+    @Test
+    public void whenDeleteAllFromeTableAndPriteTable() {
+        storage.delete();
+        storage.print();
+    }
+
+    @Test
+    public void whenCheckIfTableExist() {
+        assertTrue(storage.ifTableExist());
+    }
 }
