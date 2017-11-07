70723862ff8132b10441c954f75ea0b4fa077302
Anton <anton415@gmail.com>
2017-11-07 22:06:29 +0300

    Add HTML page for ServletGetUser.java

--- Part_Nine_Servlet/src/main/java/ru.aserdyuchenko/servlet/ServletGetUser.java
+++ Part_Nine_Servlet/src/main/java/ru.aserdyuchenko/servlet/ServletGetUser.java
@@ -10,7 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Anton Serdyuchenko. anton415@gmail.com
@@ -27,10 +30,44 @@ public class ServletGetUser extends HttpServlet {
             Class.forName("org.postgresql.Driver");
             Storage storage = new Storage();
             Map<String, User> users = storage.get();
-            writer.append(users.toString());
+            Set<Map.Entry<String, User>> list = users.entrySet();
+
+//            writer.append(users.toString());
+            writer.append(
+                    "<!DOCTYPE html>" +
+                    "<html lang=\"en\">" +
+                    "<head>" +
+                    "   <meta charset=\"UTF-8\">" +
+                    "   <title></title>" +
+                    "</head>" +
+                    "<body>" +
+                    "<table style=\"width:100%\">" +
+                    "<tr>" +
+                    "<th>Name</th> " +
+                    "<th>Email</th>" +
+                    "<th>Create date</th>" +
+                    "</tr>");
+
+            for (Map.Entry<String, User> user : list) {
+                writer.append("<tr><td>")
+                        .append(user.getValue().getName())
+                        .append("</td><td>")
+                        .append(user.getValue().getEmail())
+                        .append("</td><td>")
+                        .append(user.getValue().getCreateDate())
+                        .append("</td></tr>");
+            }
+
+            writer.append("</table>" +
+                    "</form>" +
+                    "</body>" +
+                    "</html>");
+
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            writer.flush();
         }
-        writer.flush();
     }
 }
