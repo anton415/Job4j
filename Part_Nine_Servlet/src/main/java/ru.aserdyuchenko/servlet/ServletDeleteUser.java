b175c1ea768a9deee9d31922578acee3ac43c9e1
Anton <anton415@gmail.com>
2017-11-16 20:50:35 +0300

    Add HTML page for ServletDeleteUser.java

--- Part_Nine_Servlet/src/main/java/ru.aserdyuchenko/servlet/ServletDeleteUser.java
+++ Part_Nine_Servlet/src/main/java/ru.aserdyuchenko/servlet/ServletDeleteUser.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,6 +19,59 @@ import java.util.Map;
  */
 public class ServletDeleteUser extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(ServletDeleteUser.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(response.getOutputStream());
+        try {
+            Class.forName("org.postgresql.Driver");
+            Storage storage = new Storage();
+            List<User> users = storage.getList();
+            writer.append(
+                    "<!DOCTYPE html>" +
+                            "<html lang=\"en\">" +
+                            "<head>" +
+                            "   <meta charset=\"UTF-8\">" +
+                            "   <title></title>" +
+                            "</head>" +
+                            "<body>" +
+                            "<table style=\"width:100%\">" +
+                            "<tr>" +
+                            "<th>Login</th> " +
+                            "<th>Name</th> " +
+                            "<th>Email</th>" +
+                            "<th>Create date</th>" +
+                            "</tr>");
+
+            for (User user : users) {
+                writer.append("<tr><td>")
+                        .append(user.getLogin())
+                        .append("</td><td>")
+                        .append(user.getName())
+                        .append("</td><td>")
+                        .append(user.getEmail())
+                        .append("</td><td>")
+                        .append(user.getCreateDate())
+                        .append("</td></tr>");
+            }
+
+            writer.append("</table>" +
+                    "</form>" +
+                    "<br/>" +
+                    "<br/>" +
+                    "<form action='"+request.getContextPath()+"/deleteUser' method='post'>" +
+                    "Login : <input type='text' name='login'/><br/>" +
+                    "<input type='submit'>" +
+                    "</form>" +
+                    "</body>" +
+                    "</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            writer.flush();
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
