939208e93f436e7b5e312b94648c5bcb19b4ee9e
Anton Serdyuchenko <a.serdyuchenko@yahoo.com>
2018-07-07 11:58:15 +0300

    Add realisation of User object and simple test.

--- Pre-middle/Part_2_Hibernate/car_sale/src/main/java/ru/aserdyuchenko/object/User.java
+++ Pre-middle/Part_2_Hibernate/car_sale/src/main/java/ru/aserdyuchenko/object/User.java
@@ -1,4 +1,69 @@
 package ru.aserdyuchenko.object;
 
+import java.util.ArrayList;
+
 public class User {
+    private Integer id;
+    private String name;
+    private ArrayList<Car> cars = new ArrayList<Car>(3);
+
+    public User(String name) {
+        // TODO: Add id's generation.
+        this.id = 0;
+        this.name = name;
+    }
+
+    public User(String name, Car car) {
+        // TODO: Add id's generation.
+        this.id = 0;
+        this.name = name;
+        this.cars.add(car);
+    }
+
+    public User(String name, Car... cars) {
+        // TODO: Add id's generation.
+        this.id = 0;
+        this.name = name;
+        for (Car car : cars) {
+            this.cars.add(car);
+        }
+    }
+
+    public User(String name, ArrayList<Car> cars) {
+        // TODO: Add id's generation.
+        this.id = 0;
+        this.name = name;
+         this.cars = cars;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String toString() {
+        String userToString;
+        if(this.cars.isEmpty()) {
+            userToString = name;
+        } else {
+            userToString = name + ", cars=" + cars;
+        }
+        return userToString;
+    }
 }
--- Pre-middle/Part_2_Hibernate/car_sale/src/test/java/ru/aserdyuchenko/object/UserTest.java
+++ Pre-middle/Part_2_Hibernate/car_sale/src/test/java/ru/aserdyuchenko/object/UserTest.java
@@ -1,7 +1,44 @@
 package ru.aserdyuchenko.object;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class UserTest {
 
+    @Test
+    public void whenCreateSimpleUserObjectAndGetLikeStringWhenUserHasNotCar() {
+        User user = new User("Anton");
+        assertThat(user.toString(), is("Anton"));
+    }
+
+    @Test
+    public void whenCreateSimpleUserObjectAndGetLikeStringWhenUserHasCar() {
+        Car car = new Car("Renault", "Sandero", 2011, 121000L, "white");
+        User user = new User("Anton", car);
+        assertThat(user.toString(), is("Anton, cars=[Renault, Sandero, 2011, 121000, white]"));
+    }
+
+    @Test
+    public void whenCreateSimpleUserObjectAndGetLikeStringWhenUserHasTwoCars() {
+        Car sandero = new Car("Renault", "Sandero", 2011, 121000L, "white");
+        Car huracan = new Car("Lamborghini", "Huracan", 2015, 19000L, "orange");
+        User user = new User("Anton", sandero, huracan);
+        assertThat(user.toString(),
+                is("Anton, cars=[Renault, Sandero, 2011, 121000, white, Lamborghini, Huracan, 2015, 19000, orange]"));
+    }
+
+    @Test
+    public void whenCreateSimpleUserObjectAndGetLikeStringWhenUserHasTwoCarsLikeList() {
+        Car sandero = new Car("Renault", "Sandero", 2011, 121000L, "white");
+        Car huracan = new Car("Lamborghini", "Huracan", 2015, 19000L, "orange");
+        ArrayList<Car> cars = new ArrayList<Car>(Arrays.asList(sandero, huracan));
+        User user = new User("Anton", cars);
+        assertThat(user.toString(),
+                is("Anton, cars=[Renault, Sandero, 2011, 121000, white, Lamborghini, Huracan, 2015, 19000, orange]"));
+    }
 }
